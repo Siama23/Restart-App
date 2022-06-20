c4da2812a44280c85b720ba1c60a774e95e5aae8
Alessia Masola <alessiamasola23@gmail.com>
2022-06-20 17:26:51 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Alessia Masola on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
