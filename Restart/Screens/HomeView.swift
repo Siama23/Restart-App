9de87b0800bf341c9344327a57e20de9efb2edaf
Alessia Masola <alessiamasola23@gmail.com>
2022-06-20 20:47:36 +0900

    first commit

---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Alessia Masola on 20/06/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Home")
+                .font(.largeTitle)
+            Button(action:{
+                isOnboardingViewActive = true
+            }) {
+                Text("Restart")
+            }
+        } //: VSTACK
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
